f0a6aeb12cd8d1676ba1799f3cdfd9e38ddc473d
Bliss Man <blissman@gmail.com>
2019-01-19 18:23:57 -0500

    #1 add test for case where ads.js doesn't load

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function(config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
         frameworks: ['jasmine'],
         files: [
             'src/**/*.js',
--- test/ads-enabled.spec.js
+++ test/ads-enabled.spec.js
@@ -1,18 +1,14 @@
 describe("detect-adblock", function() {
 
-    beforeAll(function() {
-        delete window.detectAB.ads;
-    });
-
     it("detectAB.detect should return false if the callback parameter is not a function", function() {
         expect(window.detectAB.detect("string")).toBe(false);
     });
 
-    it("detectAB.detect should return false if a callback is a function, and ads are disabled", function() {
+    it("detectAB.detect should return true if a callback is a function, and ads are enabled", function() {
         const callback = () => {
             return;
         };
-        expect(window.detectAB.detect(callback)).toBe(false);
+        expect(window.detectAB.detect(callback)).toBe(true);
     });
 
 });
