8819ccff86cc914c45953ca8bc1406731062c8d6
Bliss Man <blissman@gmail.com>
2019-03-16 15:10:36 +0900

    various linting bug fixes

--- src/detectAB.js
+++ src/detectAB.js
@@ -1,11 +1,13 @@
 window.detectAB = (() => {
     return {
         makeRequest: (url, method) => {
-            let request = new XMLHttpRequest();
+            const request = new XMLHttpRequest();
 
             return new Promise((resolve, reject) => {
                 request.onreadystatechange = () => {
-                    if (request.readyState !== 4) return;
+                    if (request.readyState !== 4) {
+                        return;
+                    }
                     if (request.status >= 200 && request.status < 300) {
                         resolve(request);
                     } else {
@@ -15,7 +17,7 @@ window.detectAB = (() => {
                         });
                     }
                 };
-                request.open(method || 'GET', url, true);
+                request.open(method || "GET", url, true);
                 request.send();
             });
         },
@@ -26,7 +28,7 @@ window.detectAB = (() => {
             };
             window.detectAB.ads = !!state;
         }
-    }
+    };
 })();
 
 window.detectAB.makeRequest("/ads.js").then((data) => {
